cd05f2e9a6b8c427b515e2591b71e79f567ee6ba
raalse <seteanu.radu@gmail.com>
2016-12-02 11:21:25 +0100

    P-3.8

---
+++ softwaresystems/src/ss/week3/Format.java
@@ -0,0 +1,15 @@
+package ss.week3;
+
+public class Format {
+	
+	public static void main(String[] args) {
+		printLine("text1", 1.00);
+		printLine("other text", -12.12);
+		printLine("something", 0.20);
+	}
+
+	public static void printLine(String text, double amount) {
+		System.out.format("%-10s" + "\t" + "%10.2f" + "\n", text, amount);
+	}
+
+}
