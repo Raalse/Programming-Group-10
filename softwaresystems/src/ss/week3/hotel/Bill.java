4628205e437a1b2a0c66e3bda67cd83d69e010c9
raalse <seteanu.radu@gmail.com>
2016-12-02 12:21:49 +0100

    Bill.java

---
+++ softwaresystems/src/ss/week3/hotel/Bill.java
@@ -0,0 +1,33 @@
+package ss.week3.hotel;
+
+public class Bill {
+	
+	public Item item;
+	public java.io.PrintStream stream;
+	private double sum = 0;
+	
+	public static interface Item {
+		double getAmount();
+	}
+
+	public Bill(java.io.PrintStream theOutStream) {
+		stream = theOutStream;
+	}
+	
+	public double getSum() {
+		return sum;
+	}
+	
+	public void close() {
+		stream.print(sum);
+	}
+	
+	public void newItem(Bill.Item item) {
+		this.item = item;
+		sum += item.getAmount();
+		if (stream != null) {
+			stream.print(item);
+		}
+	}
+
+}
