bf009d450c98abaca282988bc4c515bef9767d95
raalse <seteanu.radu@gmail.com>
2016-12-03 18:19:50 +0100

    TestBill is implemented and fully functional

--- softwaresystems/src/ss/week3/test/BillTest.java
+++ softwaresystems/src/ss/week3/test/BillTest.java
@@ -1,27 +1,30 @@
 package ss.week3.test;
 
-import static org.junit.Assert.*;
-
-import ss.week3.hotel.Bill;
-import ss.week3.hotel.AnItem;
+import ss.week3.hotel.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BillTest {
 
 	private Bill testBill;
 	private AnItem testItem;
+	private AnItem testItem2;
 	
 	@Before
 	public void setUp() throws Exception {
 		testBill = new Bill(null);
 		testItem = new AnItem("Whiskey", 10);
+		testItem2 = new AnItem("ChocolateBar", 4.99);
 	}
 
 	@Test
-	public void testAnItem() { //Class implement of Item
-		assertEquals(10, testItem.getAmount());
-		assertEquals("Whiskey", testItem.toString());
+	public void testBill() throws Exception {
+		assertEquals(0, testBill.getSum(), 0);
+		testBill.newItem(testItem);
+		assertEquals(10, testBill.getSum(), 0);
+		testBill.newItem(testItem2);
+		assertEquals(14.99, testBill.getSum(), 0);
 	}
-
 }
