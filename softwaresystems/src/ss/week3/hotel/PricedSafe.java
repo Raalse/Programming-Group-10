dad3ca04e541c83f18c20835c8101629d3df4bfa
raalse <seteanu.radu@gmail.com>
2016-12-04 11:52:49 +0100

    Made PricedSafe

---
+++ softwaresystems/src/ss/week3/hotel/PricedSafe.java
@@ -0,0 +1,18 @@
+package ss.week3.hotel;
+
+import ss.week3.hotel.Bill.Item;
+
+public class PricedSafe extends Safe implements Item {
+
+	public PricedSafe(String password) {
+		super(password);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
