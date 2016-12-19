60a7f9892a06ce0c33f066328d552c30fa721fe7
raalse <seteanu.radu@gmail.com>
2016-12-19 15:42:48 +0100

    -P-5.4 done, missing JML

--- softwaresystems/src/ss/week5/MapUtil.java
+++ softwaresystems/src/ss/week5/MapUtil.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.Arrays;
 //import java.util.Iterator;
 import java.util.Collection;
 
@@ -33,25 +34,76 @@ public class MapUtil {
     	}
         return true;
     }
-    public static <K, V> 
-           boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
-        // TODO: implement, see exercise P-5.2
-        return false;
-    }
-    public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.3
-        return null;
+	
+	public static <K, V> boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
+		Set<K> set = map.keySet();
+		for (K set2: set) {
+		 	if (!map.containsKey(set2)) {
+		 		return false;
+		 	}
+		}
+		for (V range2: range) {
+		 	if (!map.containsValue(range2)) {
+		 		return false;
+		 	}
+		}
+		return true;
 	}
+	
+	public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
+        Map<V, Set<K>> output = new HashMap<V, Set<K>>();
+        Set<K> set = map.keySet();
+        for (K set2:set) {
+        	V val = map.get(set2);
+        	if (output.containsKey(val)) {
+        		Set<K> val2 = output.get(val);
+        		val2.add(set2);
+        		output.put(val, val2);
+        	} else {
+        		output.put(val, new HashSet<K>(Arrays.asList(set2)));
+        	}
+        }
+        return output;
+	}
+	
 	public static <K, V> Map<V, K> inverseBijection(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.3
-        return null;
+		Map<V, K> output = new HashMap<V, K>();
+        Set<K> set = map.keySet();
+        Collection<V> setv = map.values();
+        if (!(isOneOnOne(map) && isSurjectiveOnRange(map, new HashSet<V>(setv)))) {
+        	return null;
+        }
+        for (K set2:set) {
+        	V val = map.get(set2);
+        	output.put(val, set2);
+        }
+        return output;
 	}
+	
 	public static <K, V, W> boolean compatible(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.4
-        return false;
+        Collection<V> setv = f.values();
+        for (V setv2:setv) {
+        	if (!g.containsKey(setv2)) {
+        		return false;
+        	}
+        }
+        return true;
 	}
+	
 	public static <K, V, W> Map<K, W> compose(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.5
-        return null;
+        if (!compatible(f, g)) {
+        	return null;
+        }
+        Map<K, W> output = new HashMap<K, W>();
+        Set<K> set = f.keySet();
+        Set<V> setv = g.keySet();
+        for (K set2:set) {
+        	for (V setv2:setv) {
+        		if (setv2 == f.get(set2)) {
+        			output.put(set2, g.get(setv2));
+        		}
+        	}
+        }
+        return output;
 	}
 }
---
+++ softwaresystems/src/ss/week5/tictactoe/TicTacToe.java
@@ -0,0 +1,17 @@
+package ss.week5.tictactoe;
+
+/**
+ * Executable class for the game Tic Tac Toe. The game can be played against the
+ * computer. Lab assignment Module 2
+ * 
+ * @author Theo Ruys
+ * @version $Revision: 1.4 $
+ */
+public class TicTacToe {
+    public static void main(String[] args) {
+    	Player player1 = new HumanPlayer(args[0], Mark.XX);
+    	Player player2 = new HumanPlayer(args[1], Mark.OO);
+        Game match = new Game(player1, player2);
+        match.start();
+    }
+}
