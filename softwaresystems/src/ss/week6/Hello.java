2a0fde695988a3a57d28c928f62b52dc9b5fc2a5
raalse <seteanu.radu@gmail.com>
2016-12-21 10:26:43 +0100

    -P-6.1 done

---
+++ softwaresystems/src/ss/week6/Hello.java
@@ -0,0 +1,30 @@
+package ss.week6;
+
+import java.util.Scanner;
+
+/**
+ * @author raalse
+ *
+ */
+public class Hello {
+	
+	/**
+	 * 
+	 */
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String name;
+		while (in.hasNextLine() && !(name = in.nextLine()).equals("")) {
+			System.out.println("Hello " + name);
+		}
+
+	}
+
+}
